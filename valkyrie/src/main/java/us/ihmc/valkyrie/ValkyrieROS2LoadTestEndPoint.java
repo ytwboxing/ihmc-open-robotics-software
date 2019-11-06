3f1449f6a1f9339f868232cd5277e663becaacee
Valkyrie <valkyrie@nasa.gov>
2019-11-06 13:50:06 -0600

    Bunch of cleanup before complexifying the setup.

---
+++ valkyrie/src/main/java/us/ihmc/valkyrie/ValkyrieROS2LoadTestEndPoint.java
@@ -0,0 +1,227 @@
+package us.ihmc.valkyrie;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Function;
+
+import controller_msgs.msg.dds.KinematicsToolboxOutputStatus;
+import controller_msgs.msg.dds.RobotConfigurationData;
+import controller_msgs.msg.dds.StereoVisionPointCloudMessage;
+import us.ihmc.commons.Conversions;
+import us.ihmc.communication.IHMCROS2Publisher;
+import us.ihmc.communication.ROS2Tools;
+import us.ihmc.euclid.tools.EuclidCoreRandomTools;
+import us.ihmc.pubsub.DomainFactory.PubSubImplementation;
+import us.ihmc.ros2.Ros2Node;
+
+@SuppressWarnings({"rawtypes", "unchecked"})
+public class ValkyrieROS2LoadTestEndPoint
+{
+   public final String[] pubTopicNames, subTopicNames;
+
+   private final Random random = new Random();
+   private final Ros2Node pubROS2node;
+   private final ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
+
+   private final List<IHMCROS2Publisher> pubs;
+   private final List<AtomicReference> subOutputs;
+
+   private final EndPointParameters endPointParameters;
+
+   public ValkyrieROS2LoadTestEndPoint(EndPointParameters endPointParameters, EndPointParameters otherEndPointParameters)
+   {
+      this.endPointParameters = endPointParameters;
+      Runtime.getRuntime().addShutdownHook(new Thread(() -> shutdown()));
+
+      pubROS2node = ROS2Tools.createRos2Node(PubSubImplementation.FAST_RTPS, "ros2_load_test_pub_node_" + endPointParameters.getName());
+      pubTopicNames = topicNames(endPointParameters);
+      subTopicNames = topicNames(otherEndPointParameters);
+      int numberOfPubs = endPointParameters.getNumberOfPubs();
+      int numberOfSubs = endPointParameters.getNumberOfSubsPerTopic() * otherEndPointParameters.getNumberOfPubs();
+      pubs = new ArrayList<>(numberOfPubs);
+      subOutputs = new ArrayList<>(numberOfSubs);
+
+      for (int i = 0; i < numberOfPubs; i++)
+      {
+         pubs.add(ROS2Tools.createPublisher(pubROS2node, endPointParameters.getPubType(), pubTopicNames[i]));
+      }
+
+      for (int i = 0; i < otherEndPointParameters.getNumberOfPubs(); i++)
+      {
+         String topicName = subTopicNames[i];
+         Class subType = otherEndPointParameters.getPubType();
+
+         for (int j = 0; j < endPointParameters.getNumberOfSubsPerTopic(); j++)
+         {
+            AtomicReference subOutput = new AtomicReference<>(null);
+            subOutputs.add(subOutput);
+            ROS2Tools.createCallbackSubscription(pubROS2node, subType, topicName, m -> subOutput.set(m.takeNextData()));
+         }
+      }
+
+      long pubUpdatePeriodInMs = endPointParameters.getPubUpdatePeriodInMs();
+      long subUpdatePeriodInMs = endPointParameters.getSubUpdatePeriodInMs();
+
+      executorService.scheduleAtFixedRate(this::update, 0, pubUpdatePeriodInMs, TimeUnit.MILLISECONDS);
+      executorService.scheduleAtFixedRate(this::printSubOutputs, 0, subUpdatePeriodInMs, TimeUnit.MILLISECONDS);
+   }
+
+   private void update()
+   {
+      for (int i = 0; i < pubs.size(); i++)
+      {
+         pubs.get(i).publish(endPointParameters.nextMessage(random));
+      }
+   }
+
+   private void printSubOutputs()
+   {
+      System.out.println();
+      System.out.println();
+      System.out.println("Most recent outputs at " + Conversions.nanosecondsToSeconds(System.nanoTime()));
+
+      for (int i = 0; i < subOutputs.size(); i++)
+      {
+         Object message = subOutputs.get(i).getAndSet(null);
+         System.out.println(message == null ? "null" : message.getClass().getSimpleName());
+      }
+   }
+
+   private void shutdown()
+   {
+      executorService.shutdown();
+      pubROS2node.destroy();
+      System.exit(0);
+   }
+
+   private String[] topicNames(EndPointParameters parameters)
+   {
+      String[] topicNames = new String[parameters.getNumberOfPubs()];
+
+      for (int i = 0; i < parameters.getNumberOfPubs(); i++)
+         topicNames[i] = parameters.getName() + "_topic_" + i;
+
+      return topicNames;
+   }
+
+   public static class EndPointParameters
+   {
+      private final String name;
+      private final int numberOfPubs;
+      private final int numberOfSubsPerTopic;
+      private final long pubUpdatePeriodInMs;
+      private final long subUpdatePeriodInMs;
+      private final Function<Random, Object> randomGenerator;
+
+      public EndPointParameters(String name, int numberOfPubs, int numberOfSubsPerTopic, long pubUpdatePeriodInMs, long subUpdatePeriodInMs,
+                                Function<Random, Object> randomGenerator)
+      {
+         this.name = name;
+         this.numberOfPubs = numberOfPubs;
+         this.numberOfSubsPerTopic = numberOfSubsPerTopic;
+         this.pubUpdatePeriodInMs = pubUpdatePeriodInMs;
+         this.subUpdatePeriodInMs = subUpdatePeriodInMs;
+         this.randomGenerator = randomGenerator;
+
+      }
+
+      public String getName()
+      {
+         return name;
+      }
+
+      public int getNumberOfPubs()
+      {
+         return numberOfPubs;
+      }
+
+      public int getNumberOfSubsPerTopic()
+      {
+         return numberOfSubsPerTopic;
+      }
+
+      public long getPubUpdatePeriodInMs()
+      {
+         return pubUpdatePeriodInMs;
+      }
+
+      public long getSubUpdatePeriodInMs()
+      {
+         return subUpdatePeriodInMs;
+      }
+
+      public Object nextMessage(Random random)
+      {
+         return randomGenerator.apply(random);
+      }
+
+      public Class<?> getPubType()
+      {
+         return nextMessage(new Random()).getClass();
+      }
+   }
+
+   public static EndPointParameters linkEndPoint()
+   {
+      return new EndPointParameters("link", 1, 1, 1, 1000, ValkyrieROS2LoadTestEndPoint::nextRobotConfigurationData);
+   }
+
+   public static EndPointParameters zeldaEndPoint()
+   {
+      return new EndPointParameters("zelda", 1, 10, 16, 1000, ValkyrieROS2LoadTestEndPoint::nextStereoVisionPointCloudMessage);
+   }
+
+   public static KinematicsToolboxOutputStatus nextKinematicsToolboxOutputStatus(Random random)
+   {
+      KinematicsToolboxOutputStatus message = new KinematicsToolboxOutputStatus();
+
+      message.getDesiredRootOrientation().set(EuclidCoreRandomTools.nextQuaternion(random));
+      message.getDesiredRootTranslation().set(EuclidCoreRandomTools.nextVector3D(random));
+      message.getDesiredRootAngularVelocity().set(EuclidCoreRandomTools.nextVector3D(random));
+      message.getDesiredRootLinearVelocity().set(EuclidCoreRandomTools.nextVector3D(random));
+
+      for (int i = 0; i < 40; i++)
+      {
+         message.getDesiredJointAngles().add(random.nextFloat());
+         message.getDesiredJointVelocities().add(random.nextFloat());
+      }
+
+      return message;
+   }
+
+   public static RobotConfigurationData nextRobotConfigurationData(Random random)
+   {
+      RobotConfigurationData message = new RobotConfigurationData();
+
+      message.getRootTranslation().set(EuclidCoreRandomTools.nextVector3D(random));
+      message.getRootOrientation().set(EuclidCoreRandomTools.nextQuaternion(random));
+
+      for (int i = 0; i < 40; i++)
+      {
+         message.getJointAngles().add(random.nextFloat());
+         message.getJointVelocities().add(random.nextFloat());
+         message.getJointTorques().add(random.nextFloat());
+      }
+      return message;
+   }
+
+   public static StereoVisionPointCloudMessage nextStereoVisionPointCloudMessage(Random random)
+   {
+      StereoVisionPointCloudMessage message = new StereoVisionPointCloudMessage();
+
+      for (int i = 0; i < 200000; i++)
+      {
+         message.getPointCloud().add(random.nextFloat());
+         message.getPointCloud().add(random.nextFloat());
+         message.getPointCloud().add(random.nextFloat());
+         message.getColors().add(random.nextInt());
+      }
+
+      return message;
+   }
+}
--- valkyrie/src/main/java/us/ihmc/valkyrie/ValkyrieROS2LoadTestZelda.java
+++ valkyrie/src/main/java/us/ihmc/valkyrie/ValkyrieROS2LoadTestZelda.java
@@ -1,115 +1,12 @@
 package us.ihmc.valkyrie;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicReference;
-
-import controller_msgs.msg.dds.KinematicsToolboxOutputStatus;
-import controller_msgs.msg.dds.RobotConfigurationData;
-import us.ihmc.commons.Conversions;
-import us.ihmc.communication.IHMCROS2Publisher;
-import us.ihmc.communication.ROS2Tools;
-import us.ihmc.euclid.tools.EuclidCoreRandomTools;
-import us.ihmc.pubsub.DomainFactory.PubSubImplementation;
-import us.ihmc.ros2.Ros2Node;
+import static us.ihmc.valkyrie.ValkyrieROS2LoadTestEndPoint.linkEndPoint;
+import static us.ihmc.valkyrie.ValkyrieROS2LoadTestEndPoint.zeldaEndPoint;
 
 public class ValkyrieROS2LoadTestZelda
 {
-   public static final int NUMBER_OF_PUBS = 20;
-   public static final int NUMBER_OF_SUBS = ValkyrieROS2LoadTestLink.NUMBER_OF_PUBS;
-
-   public static final String[] PUB_TOPIC_NAMES = getTopicNames();
-   public static final String[] SUB_TOPIC_NAMES = ValkyrieROS2LoadTestLink.PUB_TOPIC_NAMES;
-
-   public static final long PUB_PERIOD_IN_MS = 1;
-   public static final long SUB_PERIOD_IN_MS = 1000;
-
-   public static final int MESSAGE_SIZE = 40;
-
-   private final Random random = new Random();
-   private final Ros2Node ros2node = ROS2Tools.createRos2Node(PubSubImplementation.FAST_RTPS, "ros2-load-test-zelda");
-   private final ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
-
-   private final List<IHMCROS2Publisher<RobotConfigurationData>> pubs = new ArrayList<>(NUMBER_OF_PUBS);
-   private final List<AtomicReference<KinematicsToolboxOutputStatus>> subOutputs = new ArrayList<>(NUMBER_OF_SUBS);
-
-   public ValkyrieROS2LoadTestZelda()
-   {
-      Runtime.getRuntime().addShutdownHook(new Thread(() -> shutdown()));
-
-      for (int i = 0; i < NUMBER_OF_PUBS; i++)
-      {
-         pubs.add(ROS2Tools.createPublisher(ros2node, RobotConfigurationData.class, PUB_TOPIC_NAMES[i]));
-      }
-
-      for (int i = 0; i < NUMBER_OF_SUBS; i++)
-      {
-         AtomicReference<KinematicsToolboxOutputStatus> subOutput = new AtomicReference<>(null);
-         subOutputs.add(subOutput);
-         ROS2Tools.createCallbackSubscription(ros2node, KinematicsToolboxOutputStatus.class, SUB_TOPIC_NAMES[i], m -> subOutput.set(m.takeNextData()));
-      }
-
-      executorService.scheduleAtFixedRate(this::update, 0, PUB_PERIOD_IN_MS, TimeUnit.MILLISECONDS);
-      executorService.scheduleAtFixedRate(this::printSubOutputs, 0, SUB_PERIOD_IN_MS, TimeUnit.MILLISECONDS);
-   }
-
-   private void update()
-   {
-      for (int i = 0; i < NUMBER_OF_PUBS; i++)
-      {
-         pubs.get(i).publish(nextRobotConfigurationData(random, MESSAGE_SIZE));
-      }
-   }
-
-   private void printSubOutputs()
-   {
-      System.out.println();
-      System.out.println();
-      System.out.println("Most recent outputs at " + Conversions.nanosecondsToSeconds(System.nanoTime()));
-
-      for (int i = 0; i < NUMBER_OF_SUBS; i++)
-      {
-         System.out.println(subOutputs.get(i).get());
-      }
-   }
-
-   public static RobotConfigurationData nextRobotConfigurationData(Random random, int messageSize)
-   {
-      RobotConfigurationData message = new RobotConfigurationData();
-
-      message.getRootTranslation().set(EuclidCoreRandomTools.nextVector3D(random));
-      message.getRootOrientation().set(EuclidCoreRandomTools.nextQuaternion(random));
-
-      for (int i = 0; i < messageSize; i++)
-      {
-         message.getJointAngles().add(random.nextFloat());
-         message.getJointVelocities().add(random.nextFloat());
-         message.getJointTorques().add(random.nextFloat());
-      }
-      return message;
-   }
-
-   private static String[] getTopicNames()
-   {
-      String[] topicNames = new String[NUMBER_OF_PUBS];
-
-      for (int i = 0; i < NUMBER_OF_PUBS; i++)
-         topicNames[i] = "zelda_topic_" + i;
-      return topicNames;
-   }
-
-   public void shutdown()
-   {
-      executorService.shutdown();
-      ros2node.destroy();
-   }
-
    public static void main(String[] args)
    {
-      new ValkyrieROS2LoadTestZelda();
+      new ValkyrieROS2LoadTestEndPoint(zeldaEndPoint(), linkEndPoint());
    }
 }
