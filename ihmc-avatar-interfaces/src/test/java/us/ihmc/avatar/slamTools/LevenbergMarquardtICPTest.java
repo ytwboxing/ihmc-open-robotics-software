6d42de67ce2b6f68f824ccd35ef92ecac2b17c15
edile <edile@192.168.1.103>
2020-07-09 11:30:20 -0500

    More documentations.

--- ihmc-avatar-interfaces/src/test/java/us/ihmc/avatar/slamTools/LevenbergMarquardtICPTest.java
+++ ihmc-avatar-interfaces/src/test/java/us/ihmc/avatar/slamTools/LevenbergMarquardtICPTest.java
@@ -30,6 +30,9 @@ import us.ihmc.euclid.tuple2D.Point2D;
 import us.ihmc.log.LogTools;
 import us.ihmc.robotics.optimization.LevenbergMarquardtParameterOptimizer;
 
+/**
+ * see {@link LevenbergMarquardtParameterOptimizer}.
+ */
 @Tag("point-cloud-drift-correction-test")
 public class LevenbergMarquardtICPTest
 {
