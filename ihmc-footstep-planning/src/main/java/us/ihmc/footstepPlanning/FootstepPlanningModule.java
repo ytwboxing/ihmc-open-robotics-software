703d10377623a7087b099e0475df05b2fb8b0f0c
Duncan Calvert <dcalvert@ihmc.us>
2020-10-08 14:49:00 -0500

    Add some printouts to show when footstep planner is working to debug door behavior.

--- ihmc-footstep-planning/src/main/java/us/ihmc/footstepPlanning/FootstepPlanningModule.java
+++ ihmc-footstep-planning/src/main/java/us/ihmc/footstepPlanning/FootstepPlanningModule.java
@@ -131,6 +131,10 @@ public class FootstepPlanningModule implements CloseableAndDisposable
          LogTools.info("Received planning request packet but planner is currently running");
          return null;
       }
+      else
+      {
+         LogTools.info("Handling footstep planner request...");
+      }
 
       stopwatch.start();
       output.clear();
